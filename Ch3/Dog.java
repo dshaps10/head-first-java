3c0c5c4d740d7d74e4e2f63584c6b3314db0e960
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-07-23 09:49:33 -0400

    Finish basic exercise

--- Ch3/Dog.java
+++ Ch3/Dog.java
@@ -11,13 +11,13 @@ class Dog {
 		Dog[] myDogs = new Dog[3];
 		myDogs[0] = new Dog();
 		myDogs[1] = new Dog();
-		myDogs[2] = new dog1;
+		myDogs[2] = dog1;
 
 		myDogs[0].name = "Fred";
 		myDogs[1].name = "Marge";
 
 		System.out.println("last dog's name is ");
-		System.out.println(mydogs[2].name);
+		System.out.println(myDogs[2].name);
 
 		int x = 0;
 		while (x < myDogs.length) {
